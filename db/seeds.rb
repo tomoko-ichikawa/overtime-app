d59ddad1eaeb367379f18ded0fbfe2510ff5f4ed
tomokoichikawa <takator@yahoo.co.jp>
2020-08-01 15:27:20 +0900

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 @user = User.create(email: "test@test.com", password: "111111", password_confirmation: "111111", first_name: "sakura", last_name: "endo")
-
 puts "1 User created"
+
+AdminUser.create(email: "takator@yahoo.co.jp", password: "111111", password_confirmation: "111111", first_name: "tomoko", last_name: "ichikawa")
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
