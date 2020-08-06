c7e5083ec310b0bbb6a7d82e3d7ad0c635c85c13
tomokoichikawa <takator@yahoo.co.jp>
2020-08-06 12:03:40 +0900

    Added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "1 User created"
 AdminUser.create(email: "takator@yahoo.co.jp", password: "111111", password_confirmation: "111111", first_name: "tomoko", last_name: "ichikawa")
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,7 +4,7 @@ describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
 
   let(:post) do
-    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id, overtime_request: 3.5)
   end
 
   before do
@@ -33,7 +33,7 @@ describe 'navigate' do
 
     it 'has a scope so that only post creators can see their posts' do
       other_user = User.create(first_name: 'miduki', last_name: 'yamashita', email: 'miduki@test.com', password: '111111', password_confirmation: '111111')
-      post_from_other_user = Post.create(date: Date.today, rationale: 'favorite', user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: 'favorite', user_id: other_user.id, overtime_request: 3.5)
       visit posts_path
       expect(page).to_not have_content(/favorite/)
     end
@@ -52,7 +52,7 @@ describe 'navigate' do
       logout(:user)
       delete_user = FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
-      post_to_delete = Post.create(date: Date.today, rationale: "iine", user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: "iine", user_id: delete_user.id, overtime_request: 3.5)
       visit posts_path
       click_link("delete_post_#{post_to_delete.id}_from_index")
       expect(page.status_code).to eq(200)
@@ -71,13 +71,14 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-      click_on "Save"
-      expect(page).to have_content("Some rationale")
+      fill_in 'post[overtime_request]', with: 4.5
+      expect { click_on "Save" }.to change(Post, :count).by(1)
     end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
       click_on "Save"
       expect(User.last.posts.last.rationale).to eq("User Association")
     end
