dba0929e694d0bbd62e0b532c3545c31686002bf
tomokoichikawa <takator@yahoo.co.jp>
2020-08-11 19:34:11 +0900

    Created resource for Audit log with model relationship connections

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_123050) do
+ActiveRecord::Schema.define(version: 2020_08_11_101644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audit_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "status", default: 0
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_audit_logs_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.date "date"
@@ -43,5 +53,6 @@ ActiveRecord::Schema.define(version: 2020_08_06_123050) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "audit_logs", "users"
   add_foreign_key "posts", "users"
 end
