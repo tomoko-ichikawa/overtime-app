81c36531ad44f5a40ac4cfe1165340321615853c
tomokoichikawa <takator@yahoo.co.jp>
2020-08-11 20:00:44 +0900

    Created resource for Audit log with model relationship connections

---
+++ app/helpers/audti_logs_helper.rb
@@ -0,0 +1,2 @@
+module AudtiLogsHelper
+end
--- db/migrate/20200811105850_create_audit_logs.rb
+++ db/migrate/20200811105850_create_audit_logs.rb
@@ -1,4 +1,4 @@
-class CreateAuditiLogs < ActiveRecord::Migration[6.0]
+class CreateAuditLogs < ActiveRecord::Migration[6.0]
   def change
     create_table :audit_logs do |t|
       t.references :user, null: false, foreign_key: true
