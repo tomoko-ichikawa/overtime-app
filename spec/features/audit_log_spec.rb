5831e46faa77afd5c2b1354cfeabde9ca8d8bb44
tomokoichikawa <takator@yahoo.co.jp>
2020-09-08 10:17:07 +0900

    Blocked non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -19,5 +19,13 @@ describe 'AuditLog Feature' do
       visit audit_logs_path
       expect(page).to have_content(/endo/)
     end
+
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
+    end
   end
 end
