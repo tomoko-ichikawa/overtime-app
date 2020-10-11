c0f350e8be0139ecf520911277f26e3848f1d906
tomokoichikawa <takator@yahoo.co.jp>
2020-10-11 14:40:25 +0900

    init

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-// app.use('/static', express.static(__dirname + '/public'));
-// app.listen(port);
-
-if(process.env.NODE.ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'front_end', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
-  });
-  
-}
+app.use('/static', express.static(__dirname + '/public'));
+app.listen(port);
