073fc1fac9e2babbed5266509c55babe85fca049
tomokoichikawa <takator@yahoo.co.jp>
2020-10-11 13:06:10 +0900

    init

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
